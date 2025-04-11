339a1d30d4c25a94d99b4ae522e96ce18d6aaae6
smile-pinai <smile@pinai.io>
2025-04-11 15:46:22 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,26 +4,26 @@ from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("walkerAPI-mcp-server")
 
-# 官方演示方法1
+
 @mcp.tool()
 def add(a: int, b: int) -> int:
     """Add two numbers"""
     return a + b
     
-# 官方演示方法2
+
 @mcp.tool()
 def calculate_bmi(weight_kg: float, height_m: float) -> float:
     """Calculate BMI given weight in kg and height in meters"""
     return weight_kg / (height_m**2)
 
-# 挂载SSE服务器到ASGI服务器上
+
 app = Starlette(
     routes=[
         Mount('/', app=mcp.sse_app()),
     ]
 )
 
-# 启动服务器
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=3001)
